813d878670d89d6b1b45452cac44edd2fb83b1dc
Korel <reyan-korel.erben@prokube.ai>
2026-07-07 17:27:13 +0200

    new script, rename main.py

---
+++ multi_step_run_code.py
@@ -0,0 +1,62 @@
+from prokube.sandboxv2 import SandboxV2
+from prokube.sandbox import Sandbox
+
+API_URL = "https://technical-arctotherium.prokube.cloud/pkui"
+API_KEY = "pk_live_boGZR4RnpTwuDQWJedQtCu0zR9QXR-YIWhFwYfB7PFI"
+
+
+def multi_step_run_code(sandbox_api: Sandbox | SandboxV2, name: str, workspace: str, ):
+    sbx = sandbox_api.get(name=name, workspace=workspace, api_url=API_URL, api_key=API_KEY)
+    assert(sbx.status == "Running")
+    api_type = isinstance(sbx, SandboxV2) and "SandboxV2" or "Sandbox"
+    print(f"({api_type}) {sbx.name} at {sbx.workspace} workspace")
+    print("Running python code in the sandbox...")
+    sbx.run_code(language="python", code=
+    """
+        x = x if 'x' in globals() else None
+        y = y if 'y' in globals() else None
+        z = x + y + z if 'z' in globals() else x + y
+    """)
+    result = sbx.run_code(language="python", code=
+    """
+        print(z)
+    """)
+    print(f"python result: {result.stdout}")  # Output: 15, 30, 45... increments by 15 each time the code is run
+
+    print("Running bash code in the sandbox...")
+    sbx.run_code(language="bash", code=
+    """
+        x=${x:-5}
+        y=${y:-10}
+        if [ -z "$z" ]; then
+            z=$((x + y))
+        else
+            z=$((x + y + z))
+        fi
+    """)
+    sbx.run_code(language="bash", code=
+    """
+        echo $z
+    """)
+    print(f"bash result: {result.stdout}")  # Output: 15, 30, 45... increments by 15 each time the code is run
+
+    print("Running node code in the sandbox...")
+    sbx.run_code(language="node", code=
+    """
+        let x = 5;
+        let y = 10;
+        if (typeof z === 'undefined') {
+            let z = x + y;
+        } else {
+            z = x + y + z;
+        }
+    """)
+    sbx.run_code(language="node", code=
+    """
+        console.log(z);
+    """)
+    print(f"node result: {result.stdout}")  # Output: 15, 30, 45... increments by 15 each time the code is run
+
+multi_step_run_code(Sandbox, name="python-pool-tqjnq", workspace="developer1")
+print("================================")
+multi_step_run_code(SandboxV2, name="python-pool-0268d3", workspace="developer1")
