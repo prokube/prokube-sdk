e32de4e60b20e9d61ea7c390932748d91eb56bb6
Korel <reyan-korel.erben@prokube.ai>
2026-07-08 16:02:34 +0200

    updates

--- from_pool.py
+++ from_pool.py
@@ -4,24 +4,28 @@ import time
 
 
 def from_pool(sbx_api, pool):
-    t = time.perf_counter()
+    print("Testing from_pool() with", sbx_api.__name__, "and pool:", pool)
+    t1 = time.perf_counter()
     with sbx_api.from_pool(
         pool=pool,
         workspace="developer1",
         api_url="https://technical-arctotherium.prokube.cloud/pkui",
         api_key="pk_live_boGZR4RnpTwuDQWJedQtCu0zR9QXR-YIWhFwYfB7PFI",
     ) as sbx:
-        print(
-            sbx.run_code("""
+        time_taken = time.perf_counter() - t1
+        print(f"Acquire **ready** sandbox: {time_taken:.6f}s")
+        t2 = time.perf_counter()
+        sbx.run_code("""
             print("Hello, world!")
             """).stdout
-        )
+        print(f"Run code time: {time.perf_counter() - t2:.6f}s")
+        print(f"Total time to run code: {time.perf_counter() - t1:.6f}s")
+    time_taken = time.perf_counter() - t1
+    print(f"Total time: {time_taken:.6f}s")
 
-    time_taken = time.perf_counter() - t
-    api = "SandboxV2" if sbx_api == SandboxV2 else "Sandbox"
-    print(f"Time taken : {time_taken:.6f} seconds for {api} from_pool({pool})")
-
-
-from_pool(SandboxV2, "hibernated")
-from_pool(SandboxV2, "running")
-from_pool(Sandbox, "v1pool")
+for i in range(20):
+    print("==============================")
+    print(f"Iteration {i+1}/20")
+    from_pool(SandboxV2, "gpool")
+# from_pool(SandboxV2, "running")
+# from_pool(Sandbox, "v1pool")
